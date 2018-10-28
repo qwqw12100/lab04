1914ff2f423b137f5a221d1c28ec6e0517ad6a13
qwqw12100 <qwqw12100@github.com>
2018-10-28 07:26:47 +0000

    “setup”

--- irtester.c
+++ irtester.c
@@ -11,19 +11,19 @@ int main(int argc, char *argv[])
   pinMode(2, OUTPUT);
   while(1) {
     printf("Waiting for reset\n");
-    
-	while(digitalRead(0) == 1){
+
+	while(digitalRead(0) == 1)
 	digitalWrite (1, HIGH) ;    
-    digitalWrite (2,  LOW) ;
+        digitalWrite (2,  LOW) ;
 	printf("Waiting for event\n");	
-	}
+	
     
-    while(digitalRead(0) == 0){		
-		digitalWrite (2, HIGH) ;    
+        while(digitalRead(0) == 0)		
+	digitalWrite (2, HIGH) ;    
         digitalWrite (1,  LOW) ;
-		ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/123", "my1", "my 2", "my 33333");
-		printf("Alarm\n");		
-	}   
+	ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/123", "my1", "my 2", "my 33333");
+	printf("Alarm\n");		
+	   
   }
   /*NOTREACHED*/
   return 0 ;
